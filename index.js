883b42597c26f78c04112f78ee8c08e6b7be79ff
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2021-07-11 08:07:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const rateLimit = require("express-rate-limit");
 const connectDB = require('./server/database/connection');
-connectDB();
 
 const app = express();
 const PORT = process.env.PORT;
 
-const limiter = rateLimit({
-	windowMs: 1000 * 60 * 60 , // 1 hr
-	max: 250, // limit each IP to 250 requests per windowMs
-  message:
-    "Too many requests created from this IP, please try again after an hour"
-})
-app.use(limiter)
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 
